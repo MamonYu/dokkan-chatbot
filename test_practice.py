0ac874f96cf9805a4f0a6b5eea3553022bb6ee1f
Mamon <mamonyousef1@gmail.com>
2026-07-08 00:41:38 +0300

    tried some parts in the stage 1

--- test_practice.py
+++ test_practice.py
@@ -1,71 +1,5 @@
-# # # test 1
-# # # write a function that takes a list of numbers 
-
-# # def evenNum(userNum):
-# #     boxOfEven = []
-# #     for x in userNum:
-# #         if (x % 2 == 0):
-# #             boxOfEven.append(x)
-# #         else:
-# #             continue
-
-# #     return boxOfEven
-
-# # newX = evenNum([1,2,3,4,5,6])
-# # print(newX)
-
-
-# # # test 2
-# # # You have this:
-# # unit = {"name": "Goku", "type": "AGL", "rarity": "LR"}
-# # # Write code that prints "Goku is an AGL LR unit"
-# # # using the dictionary values, not hardcoded text
-
-# # print(f'{unit["name"]} is an {unit["type"]} LR unit ')
-
-
-
-
-# # # task 3
-# # # # Create a class called Unit with name, type, and rarity
-# # # # Then create two Unit objects and print both of their names
-
-# # class Unit:
-# #     def __init__(self, name, type, rarity ):
-# #         self.name = name
-# #         self.type = type
-# #         self.rarity = rarity
-
-
-# # unit1 = Unit('Goku' , 'AGL' , 'LR')
-# # unit2 = Unit('Vegeta' , 'PHY' , 'SSR')
-
-# # print(unit1.name)
-# # # print(unit2.name)
-
-# # my_team = [
-# #             {"name": "Goku", "type": "AGL", "rarity": "LR"}, 
-# #             {"name": "Vegeta", "type": "AGL", "rarity": "LR"},
-# #             {"name": "Picolo", "type": "PHY", "rarity": "LR"},
-# #             {"name": "Nappa", "type": "TEQ", "rarity": "LR"},
-# #             {"name": "Raditz", "type": "STR", "rarity": "LR"}
-# #     ]
-# # # You have a list of dictionaries (like Test 2's unit, but 5 of them)
-# # # Write code that returns only the units where type == "AGL"
-# # def sameType(Theteam):
-
-# #     boxOfSameType = []
-# #     for x in Theteam:
-# #         if x["type"] == "AGL":
-# #             boxOfSameType.append(x)
-
-# #     return boxOfSameType
-
-# # newBox = sameType(my_team)
-# # print(newBox)
-    
-
 # # task 5
+ # """Return all units matching the given type (case/whitespace-insensitive)."""
 import json
 import sys
 try:
@@ -136,27 +70,53 @@ except:
 # else:
 #        print(outputChar)
 
-# part 3
-def build_report(unit_list):
-       units_string = []
-       filterd_units = []
-       sortedUnits = []
-       for x in unit_list:
-            if isinstance(x["hp"], str) or not x["hp"]:
-                 continue
-            else:
-                 filterd_units.append(x)
-       sortedUnits = sorted(filterd_units , key = lambda HP : HP['hp'] , reverse = True)
-       for x in sortedUnits:
-                unit = f"{x['name']} ({x['type']}, {x['rarity']}) - {x['hp']} HP"
-                units_string.append(unit)
-       return units_string
+# # part 3
+# def build_report(unit_list):
+#        units_string = []
+#        filterd_units = []
+#        sortedUnits = []
+#        for x in unit_list:
+#             if isinstance(x["hp"], str) or not x["hp"]:
+#                  continue
+#             else:
+#                  filterd_units.append(x)
+#        sortedUnits = sorted(filterd_units , key = lambda HP : HP['hp'] , reverse = True)
+#        for x in sortedUnits:
+#                 unit = f"{x['name']} ({x['type']}, {x['rarity']}) - {x['hp']} HP"
+#                 units_string.append(unit)
+#        return units_string
 
-unitSorted = build_report(my_team)
-# part 4
-with open("report.txt" , "w" , encoding="utf-8") as file:
-      file.write("\n".join(unitSorted))
+# unitSorted = build_report(my_team)
+# # part 4
+# with open("report.txt" , "w" , encoding="utf-8") as file:
+#       file.write("\n".join(unitSorted))
 
+# STAGE 1
 
+class Unit:
+       def __init__(self , name , type , rarity, category , has_revive):
+              self.name = name
+              self.type = type
+              self.rarity = rarity
+              self.category = category
+              self.has_revive = has_revive
+       
+class Team:
+       def __init__(self):
+              self.list_of_units = []
+              
+       def add_unit(self, unit):
+              self.unit = unit
+              for unit in self.list_of_units: 
+                    self.list_of_units.append(unit)
 
+       def print_names(self):
+             return f"{self.list_of_units}"
+              
+goku = Unit("Goku", "AGL", "LR", "Pure Saiyans", True)
+vegeta = Unit("Vegeta", "AGL", "LR", "Pure Saiyans", False)
 
+my_team = Team()
+my_team.add_unit(goku)
+my_team.add_unit(vegeta)
+my_team.print_names() 
